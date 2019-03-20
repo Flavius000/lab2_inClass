c09f20e8f378b20f8e3b633fa7824482c7e9a664
PERSONAL-LPT\Flavius <flavius.ilinoiu@gmail.com>
2019-03-20 10:26:25 +0200

    First Commit

---
+++ src/test/java/Tests.java
@@ -0,0 +1,72 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import domain.Student;
+import org.junit.jupiter.api.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
+
+public class Tests {
+
+    @Test
+    public void addStudentTescase_NegativeId() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+
+        Student std = new Student("-1","TestName",10,"test@testemail.com");
+
+        try
+        {
+            service.addStudent(std);
+            assert(false) : "Student with id -1 should not be added to repository!!!";
+        }
+        catch(ValidationException ex)
+        {
+
+        }
+
+    }
+
+    @Test
+    public void addStudentTescase2() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+
+        Student std = new Student("string_id","TestName",10,"test@testemail.com");
+
+        try
+        {
+            service.addStudent(std);
+            assert(false) : "Student with string id should not be added to repository!!!";
+        }
+        catch(ValidationException ex)
+        {
+
+        }
+    }
+}
