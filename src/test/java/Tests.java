5ab149c5d684c70e92779b131fb04b57546488ef
PERSONAL-LPT\Flavius <flavius.ilinoiu@gmail.com>
2019-03-31 20:57:45 +0300

    another commit

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -69,4 +69,144 @@ public class Tests extends junit.framework.TestCase{
 
         }
     }
+
+    @Test
+    public void testaddStudentTestcase3_emptyName()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        Student std = new Student("100","",10,"test@testemail.com");
+
+        try
+        {
+            service.addStudent(std);
+            assert(false) : "Student with empty name should not be added to repository!!!";
+        }
+        catch(ValidationException ex)
+        {
+
+        }
+    }
+
+    @Test
+    public void testaddStudentTestcase4_nullName()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        Student std = new Student("100",null,10,"test@testemail.com");
+
+        try
+        {
+            service.addStudent(std);
+            assert(false) : "Student with null name should not be added to repository!!!";
+        }
+        catch(ValidationException ex)
+        {
+
+        }
+    }
+
+    @Test
+    public void testaddStudentTestcase5_negativeGroup()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        Student std = new Student("100","Gigel",-1,"test@testemail.com");
+
+        try
+        {
+            service.addStudent(std);
+            assert(false) : "Student with negative group should not be added to repository!!!";
+        }
+        catch(ValidationException ex)
+        {
+
+        }
+    }
+
+    @Test
+    public void testaddStudentTestcase6_nullEmail()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        Student std = new Student("100","Gigel",100,null);
+
+        try
+        {
+            service.addStudent(std);
+            assert(false) : "Student with null email should not be added to repository!!!";
+        }
+        catch(ValidationException ex)
+        {
+
+        }
+    }
+
+    @Test
+    public void testaddStudentTestcase7_emptyEmail()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        Student std = new Student("100","Gigel",100,"");
+
+        try
+        {
+            service.addStudent(std);
+            assert(false) : "Student with null email should not be added to repository!!!";
+        }
+        catch(ValidationException ex)
+        {
+
+        }
+    }
 }
