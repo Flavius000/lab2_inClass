06d4f671169c3b2a992e22f98d56042a4b4448ff
PERSONAL-LPT\Flavius <flavius.ilinoiu@gmail.com>
2019-05-07 21:13:58 +0300

    lab4 home

---
+++ src/test/java/TopDown.java
@@ -0,0 +1,103 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import org.joda.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
+
+public class TopDown extends junit.framework.TestCase {
+
+    public void addStudent()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        Student std = new Student("10","Name",10,"test@testemail.com");
+
+        service.addStudent(std);
+    }
+
+    public void addAssignment()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        Tema tema = new Tema("11","TestDescription", 11,  4);
+
+        try
+        {
+            service.addTema(tema);
+        }
+        catch(ValidationException ex)
+        {
+            assert(false) : "Tema should be added";
+        }
+    }
+
+    public void addGrade()
+    {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        Nota nota = new Nota("10","10","11",10, LocalDate.now());
+
+        service.addNota(nota,"foarte bine");
+    }
+
+    @Test
+    public void testAddStudent()
+    {
+        addStudent();
+    }
+
+    @Test
+    public void testAddAssignment()
+    {
+        addStudent();
+        addAssignment();
+    }
+
+    @Test
+    public void testAddGrade()
+    {
+        addStudent();
+        addAssignment();
+        addGrade();
+    }
+}
